6610acc7432be1decb2078b35aae639ce98d6330
lsh <seulee0862@gmail.com>
2024-01-10 11:10:02 +0900

    node library 사용

--- section1/src/index.js
+++ section1/src/index.js
@@ -1,6 +1,6 @@
-import { add, sub } from "./math.js";
-import multiply from "./math.js";
+import multiply, { add, sub } from "./math.js";
+import randomColor from "randomcolor";
 
-console.log(add(1, 2));
-console.log(sub(1, 2));
-console.log(multiply(1, 10));
+const color = randomColor();
+
+console.log(color);

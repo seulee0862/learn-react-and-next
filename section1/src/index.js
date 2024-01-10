1afa3be04dda5a39c9038129efa55eb836b04cd3
lsh <seulee0862@gmail.com>
2024-01-10 10:50:33 +0900

    CJS modile exports 예제

--- section1/src/index.js
+++ section1/src/index.js
@@ -1,1 +1,4 @@
-console.log("hi");
+const { add, sbb } = require("./math");
+
+console.log(add(1, 2));
+console.log(sbb(1, 2));
---
+++ section1/src/math.js
@@ -0,0 +1,13 @@
+function add(a, b) {
+  return a + b;
+}
+
+function sbb(a, b) {
+  return a - b;
+}
+
+//CJS modules export
+module.exports = {
+  add,
+  sbb,
+};

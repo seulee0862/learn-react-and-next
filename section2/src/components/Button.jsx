723d309afbb7614dd287f9fd3365672a2bb7c194
lsh <seulee0862@gmail.com>
2024-01-21 15:32:28 +0900

    section3 - event handling

--- section2/src/components/Button.jsx
+++ section2/src/components/Button.jsx
@@ -2,8 +2,13 @@ import "./Button.css"
 
 export default function Button({color, text, children}) {
 
+    const onClick = () => {
+        alert("버튼 클릭");
+    }
+
     return (
-        <button style={{
+        <button
+            onClick={onClick} style={{
             backgroundColor: color
         }}
         className="button">{children}</button>

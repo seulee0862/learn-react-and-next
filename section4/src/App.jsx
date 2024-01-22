109c70907931566870f97430ff2cfe54d2dacea0
lsh <seulee0862@gmail.com>
2024-01-23 01:22:26 +0900

    section5 - component lifecycle

--- section4/src/App.jsx
+++ section4/src/App.jsx
@@ -1,21 +1,45 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 import Viewer from "./components/Viewer"
 import Controller from './components/Controller'
+import { useRef } from 'react';
+import Even from './components/Even';
 
 function App() {
-
   const [count, setCount] = useState(0);
+  const [text, setText] = useState("");
+
+  const isMountRef = useRef(false)
+
+  useEffect(() => {
+    if (!isMountRef.current) {
+      isMountRef.current = true;
+      return;
+    }
+    //control
+  });
+
+  useEffect(() => {
+    console.log(`카운트 : ${count}, 텍스트 : ${text}`)
+  }, [count, text])
 
   const onClickButton = (value) => {
     setCount(count + value);
+  }
+
+  const onChangeText = (e) => {
+    setText(e.target.value);
   }
 
   return (
     <div className="App">
       <h1>Simple Counter</h1>
       <section>
+        <input value={text} onChange={onChangeText} />
+      </section>
+      <section>
         <Viewer count={count} />
+        {count % 2 == 0 && <Even />}
       </section>
       <section>
         <Controller onClickButton={onClickButton} />
---
+++ section4/src/components/Even.jsx
@@ -0,0 +1,3 @@
+export default function Even() {
+    return <div>짝수입니다</div>;
+}

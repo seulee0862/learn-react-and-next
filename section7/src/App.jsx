a8ef607c027e43ce7443ec87f8549ef165583a0d
lsh <seulee0862@gmail.com>
2024-01-23 14:46:33 +0900

    section7 - todo 데이터 생성

--- section7/src/App.jsx
+++ section7/src/App.jsx
@@ -2,13 +2,53 @@ import Header from './component/Header'
 import './App.css'
 import TodoEditor from './component/TodoEditor'
 import TodoList from './component/TodoList'
+import { useState } from 'react'
+import { useRef } from 'react'
+
+const mockData = [
+  {
+    id : 0,
+    isDone : true,
+    content: "React 공부하기",
+    createdDate: new Date().getTime(),
+  },
+  {
+    id : 1,
+    isDone : false,
+    content: "React 음악듣기",
+    createdDate: new Date().getTime(),
+  },
+  {
+    id : 2,
+    isDone : true,
+    content: "React 축구하기",
+    createdDate: new Date().getTime(),
+  },
+]
 
 function App() {
   
+  const [todos, setTodos] = useState(mockData);
+
+  const idRef = useRef(3);
+
+  const onCreate = (content) => {
+    const newTodo = {
+      id: idRef.current++,
+      isDone: false,
+      content,
+      createdDate : new Date().getTime()
+    }
+
+    setTodos(
+      [...todos, newTodo]
+    )
+  }
+
   return (
     <div className="App">
       <Header/>
-      <TodoEditor/>
+      <TodoEditor onCreate={onCreate} />
       <TodoList />
     </div>
   )
--- section7/src/component/TodoEditor.jsx
+++ section7/src/component/TodoEditor.jsx
@@ -1,10 +1,40 @@
+import { useState } from "react";
 import "./TodoEditor.css";
+import { useRef } from "react";
 
-export default function TodoEditor() {
+export default function TodoEditor({ onCreate }) {
+    
+    const [content, setContent] = useState("");
+    const inputRef = useRef();
+
+    const onChangeContent = (e) => {
+        setContent(e.target.value);
+    }
+
+    const onClick = () => {
+        if (content === "") {
+            inputRef.current.focus();
+            return;
+        }
+        onCreate(content);
+        setContent("");
+    }
+
+    const onKeyDown = (e) => {
+        if (e.keyCode === 13) { // 13 = enter
+            onClick();
+        }
+    }
+
     return (
         <div className="TodoEditor">
-            <input placeholder="새로운 Todo ..."/>
-            <button>추가</button>
+            <input
+                ref={inputRef}
+                value={content}
+                onChange={onChangeContent}
+                onKeyDown={}
+                placeholder="새로운 Todo ..." />
+            <button onClick={onClick}>추가</button>
         </div>
     )
 }

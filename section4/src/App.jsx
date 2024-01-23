e07d20b73be22307179d3efea1a4248476f8410c
Lee Seunghyeon <84488007+seulee0862@users.noreply.github.com>
2024-01-23 12:07:34 +0900

    Section6 (#5)
    * section5 - component lifecycle
    
    * section6 - React hook

--- section4/src/App.jsx
+++ section4/src/App.jsx
@@ -1,11 +1,22 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 import Viewer from "./components/Viewer"
 import Controller from './components/Controller'
+import Even from './components/Even';
+import useUpdate from './hooks/useUpdate';
+import useInput from './hooks/useInput';
 
 function App() {
-
   const [count, setCount] = useState(0);
+  const [text, onChangeText] = useInput("");
+
+  useUpdate(() => {
+    console.log("APP 컴포넌트 업데이트")
+  });
+
+  useEffect(() => {
+    console.log(`카운트 : ${count}, 텍스트 : ${text}`)
+  }, [count, text])
 
   const onClickButton = (value) => {
     setCount(count + value);
@@ -15,7 +26,11 @@ function App() {
     <div className="App">
       <h1>Simple Counter</h1>
       <section>
+        <input value={text} onChange={onChangeText} />
+      </section>
+      <section>
         <Viewer count={count} />
+        {count % 2 == 0 && <Even />}
       </section>
       <section>
         <Controller onClickButton={onClickButton} />

552d9b1c9670bb5188e7bf188d5013f66411dbef
lsh <seulee0862@gmail.com>
2024-01-24 14:53:43 +0900

    section9 - useMemo

--- section7/src/component/TodoList.jsx
+++ section7/src/component/TodoList.jsx
@@ -1,44 +1,58 @@
-import TodoItem from "./TodoItem"
-import "./TodoList.css"
-import { useState } from "react";
+import TodoItem from "./TodoItem";
+import "./TodoList.css";
+import { useState, useMemo } from "react";
 
-export default function TodoList({ todos, onUpdate, onDelete}) {
-    
-    const [search, setSearch] = useState("");
+export default function TodoList({ todos, onUpdate, onDelete }) {
+  const [search, setSearch] = useState("");
 
-    const onChangeSearch = (e) => {
-        setSearch(e.target.value);
+  const onChangeSearch = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const filterTodos = () => {
+    if (search === "") {
+      return todos;
     }
 
-    const filterTodos = () => {
-        if (search === "") {
-            return todos;
-        }
-
-        return todos.filter((todo) =>
-            todo.content
-                .toLowerCase()
-                .includes(search.toLowerCase())
-        );
-    }
-
-    return (
-        <div className="TodoList">
-            <h4>Todos</h4>
-            <input
-                value={search}
-                onChange={onChangeSearch}
-                placeholder="검색어를 입력하세요" />
-            <div className="todos_wrapper">
-                {filterTodos().map((todo) => (
-                    <TodoItem
-                        key={todo.id}
-                        {...todo}
-                        onUpdate={onUpdate}
-                        onDelete={onDelete}
-                    />
-                ))}
-            </div>
-        </div>
+    return todos.filter((todo) =>
+      todo.content.toLowerCase().includes(search.toLowerCase())
     );
+  };
+
+  const { totalCount, doneCount, notDoneCount } = useMemo(() => {
+    const totalCount = todos.length;
+    const doneCount = todos.filter((todo) => todo.isDone).length;
+    const notDoneCount = totalCount - doneCount;
+    return {
+      totalCount,
+      doneCount,
+      notDoneCount,
+    };
+  }, [todos]);
+
+  return (
+    <div className="TodoList">
+      <h4>Todos</h4>
+      <div>
+        <div>전체투두 : {totalCount}</div>
+        <div>완료두 : {doneCount}</div>
+        <div>미완료투두 : {notDoneCount}</div>
+      </div>
+      <input
+        value={search}
+        onChange={onChangeSearch}
+        placeholder="검색어를 입력하세요"
+      />
+      <div className="todos_wrapper">
+        {filterTodos().map((todo) => (
+          <TodoItem
+            key={todo.id}
+            {...todo}
+            onUpdate={onUpdate}
+            onDelete={onDelete}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }

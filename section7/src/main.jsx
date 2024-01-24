2f45275b28b1d8e2db9c2e7dead1ca6cd12b6858
lsh <seulee0862@gmail.com>
2024-01-24 12:49:52 +0900

    section8 - useReducer

--- section7/src/main.jsx
+++ section7/src/main.jsx
@@ -1,12 +1,7 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import A from '../exam/A.jsx'
-import B from '../exam/B.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <App />
-  //<A/>
-  <B/>
+  <App />
 )

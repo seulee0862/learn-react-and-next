ba7ef62186bbecf91dcf729d31c86bf6dd646b98
lsh <seulee0862@gmail.com>
2024-01-24 12:18:36 +0900

    section8 - reducer ex

--- section7/src/main.jsx
+++ section7/src/main.jsx
@@ -1,7 +1,12 @@
+import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import A from '../exam/A.jsx'
+import B from '../exam/B.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
+  // <App />
+  //<A/>
+  <B/>
 )

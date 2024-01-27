e490a4030cfbc5913e9732305ff2ef6e1111d6f2
lsh <seulee0862@gmail.com>
2024-01-27 15:14:46 +0900

    section11 - 레이아웃 설정(CSS)

--- section11/src/App.jsx
+++ section11/src/App.jsx
@@ -1,33 +1,21 @@
 import "./App.css";
-import { Routes, Route, Link, useNavigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Search from "./pages/Search";
 import Country from "./pages/Country";
 import NotFound from "./pages/NotFound";
+import Layout from "./components/Layout";
 
 function App() {
-  const nav = useNavigate();
-
-  const onClick = () => {
-    nav("/search");
-  };
-
   return (
-    <>
+    <Layout>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/search" element={<Search />} />
         <Route path="/country/:code" element={<Country />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
-      <div>
-        <Link to={"/"}></Link>
-        <Link to={"/search"}>Search</Link>
-        <Link to={"/country"}>Country</Link>
-
-        <button onClick={onClick}>서치 페이지로 이동</button>
-      </div>
-    </>
+    </Layout>
   );
 }
 

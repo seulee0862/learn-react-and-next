ddd16c2a8b76c54630ca737b4f741b05c66b8fd1
Lee Seunghyeon <84488007+seulee0862@users.noreply.github.com>
2024-01-28 15:03:09 +0900

    Section12~13 (#11)
    * section12 - pakage init
    
    * section12 - page routing
    
    * section12 - 페이지이동
    
    * section12 - 전체 레이아웃 설정
    
    * section12~13 - page router 레이아웃 설정

---
+++ section12/pages/index.js
@@ -0,0 +1,32 @@
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+
+export default function Home() {
+  const code = "KOR";
+  const router = useRouter();
+
+  const onClickButton = () => {
+    router.push("/search");
+  };
+  return (
+    <div>
+      Home Page
+      <div>
+        <button onClick={onClickButton}>Search 페이지로 이동</button>
+      </div>
+      <div>
+        <Link href={"/search"}>Search Page 이동</Link>
+      </div>
+      <div>
+        <Link
+          href={{
+            pathname: "/country/[code]",
+            query: { code: code },
+          }}
+        >
+          {code} 페이지로 이동
+        </Link>
+      </div>
+    </div>
+  );
+}

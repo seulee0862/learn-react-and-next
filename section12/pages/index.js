4d2ee4ce00d38542776619ea247d0914eaed78f6
lsh <seulee0862@gmail.com>
2024-01-29 00:09:19 +0900

    section17 - meta 태그 설정하기

--- section12/pages/index.js
+++ section12/pages/index.js
@@ -1,11 +1,20 @@
 import { fetchCountries } from "@/api";
 import CountryList from "@/components/CountryList";
 import Searchbar from "@/components/Searchbar";
-import { useEffect } from "react";
+import Head from "next/head";
 
 export default function Home({ countries }) {
   return (
     <>
+      <Head>
+        <title>NARAS</title>
+        <meta property="og:image" content="/thumbnail.png" />
+        <meta property="og:title" content="NARAS" />
+        <meta
+          property="og:discription"
+          content="전 세계 국가들의 정보를 확인해보세요"
+        />
+      </Head>
       <Searchbar />
       <CountryList countries={countries} />
     </>
--- section12/pages/search/index.js
+++ section12/pages/search/index.js
@@ -4,6 +4,7 @@ import Searchbar from "@/components/Searchbar";
 import SubLayout from "@/components/SubLayout";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
+import Head from "next/head";
 
 export default function Search() {
   const router = useRouter();
@@ -24,6 +25,15 @@ export default function Search() {
 
   return (
     <>
+      <Head>
+        <title>NARAS 검색</title>
+        <meta property="og:image" content="/thumbnail.png" />
+        <meta property="og:title" content="NARAS" />
+        <meta
+          property="og:discription"
+          content="전 세계 국가들의 정보를 확인해보세요"
+        />
+      </Head>
       <Searchbar q={q} />
       <CountryList countries={countries} />
     </>

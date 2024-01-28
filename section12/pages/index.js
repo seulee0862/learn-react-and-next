aa590544dae84a386e558198cbd8379281164050
lsh <seulee0862@gmail.com>
2024-01-28 17:38:38 +0900

    seciton14 - SSR(2)

--- section12/pages/index.js
+++ section12/pages/index.js
@@ -1,23 +1,24 @@
+import { fetchCountries } from "@/api";
 import { useEffect } from "react";
 
-export default function Home({ name }) {
-  console.log("HOME");
-
-  useEffect(() => {
-    console.log("COMPONENT MOUNT");
-  }, []);
-
-  return <div>{name}</div>;
+export default function Home({ countries }) {
+  return (
+    <div>
+      {countries.map((country) => (
+        <div key={country.code}>{country.commonName}</div>
+      ))}
+    </div>
+  );
 }
 
 export async function getServerSideProps() {
-  // SSR을 위해 서버 측에서 페이지 컴포넌트에게 전달할 데이터를 설정하는 함수
+  // API호출 코드 필요
 
-  console.log("getServerSideProps Called");
+  const countries = await fetchCountries();
 
   return {
     props: {
-      name: "KOREA",
+      countries,
     },
   };
 }
--- section12/pages/search/index.js
+++ section12/pages/search/index.js
@@ -1,7 +1,28 @@
+import { fetchSearchResult } from "@/api";
 import SubLayout from "@/components/SubLayout";
 
-export default function Search() {
-  return <div>Search Page</div>;
+export default function Search({ countries }) {
+  return (
+    <div>
+      {countries.map((country) => {
+        return <div key={country.code}>{country.commonName}</div>;
+      })}
+    </div>
+  );
 }
 
 Search.Layout = SubLayout;
+
+export const getServerSideProps = async (context) => {
+  const { q } = context.query;
+
+  let countries = [];
+  console.log(q);
+  if (q) {
+    countries = await fetchSearchResult(q);
+  }
+
+  return {
+    props: { countries },
+  };
+};
